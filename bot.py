dbb28955d368226cce82c6d4519b1a9de28f64af
Cameron Walsh <cmrnwlsh@gmail.com>
2023-01-25 01:39:17 -0500

    made the scheduler actually work maybe

--- bot.py
+++ bot.py
@@ -1,12 +1,22 @@
+import asyncio
+import os
 import discord
 import json
 import aiofiles
+from datetime import datetime
 from discord.ext import commands, tasks
 from random import randint
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='/', description='get swole', intents=intents)
 strong = {}
+schedule = datetime.now().replace(day=datetime.now().day + 1, hour=9, minute=0)
+iterator_lock = asyncio.Lock()
+initialized = False
+
+test_channel = discord.utils.get(client.get_all_channels(), name='the-iron-temple-test')
+channel = discord.utils.get(client.get_all_channels(), name='the-iron-temple')
+
 
 with open('token.txt') as token_file:
     token = token_file.read()
@@ -16,11 +26,9 @@ with open('log.json', 'r') as log_r:
 
 
 async def on_message(message):
-    if str(message.channel) != 'the-iron-temple':
+    if str(message.channel) not in ('the-iron-temple', 'the-iron-temple-test'):
         return
     await client.process_commands(message)
-
-client.on_message = on_message
 
 
 async def update_log():
@@ -28,51 +36,81 @@ async def update_log():
         await log_w.write(json.dumps(strong, indent=2))
 
 
-@tasks.loop(seconds=5)
-async def scheduled_pushups():
-    print('test')
-    channel = discord.utils.get(client.get_all_channels(), name='the-iron-temple')
-    string = ''
-    for member in strong:
+def roll_pushups():
+    i = 0
+
+    @tasks.loop(seconds=3)
+    async def inner_loop():
+        nonlocal i
+        if len(strong) == 0 or len(strong) == i:
+            return
+        members = list(strong.keys())
+        member = members[i]
+        n = randint(20, 30)
+        strong[member]['pushups'] += n
+        await update_log()
+
         user = discord.utils.get(
             client.users,
             name=''.join(member[:-5]),
             discriminator=''.join(member[-4::]))
 
-        pushup_count = randint(10, 30)
-        string += f'{user.mention} has been assigned {pushup_count} pushups\n'
-        strong[member]['pushups'] += pushup_count
+        await test_channel.send(f'{user.mention} has been assigned {n} pushups')
+        inner_loop.change_interval(minutes=(12*60)/len(members))
+        async with iterator_lock:
+            i += 1
+
+    return inner_loop
+
+
+@tasks.loop(hours=24)
+async def daily_reset():
+    if len(strong) == 0:
+        return
+    print('test')
+    for member in strong:
+        strong[member]['rolls'] += 1
+        strong[member]['pushups'] = 0
+        async with iterator_lock:
+            roll_pushups.i = 0
 
     await update_log()
-    await channel.send(string)
-
-
-@client.event
-async def on_ready():
-    scheduled_pushups.start()
-    print(client.guilds)
-
-
-@client.command()
-async def pushups(ctx):
-    if str(ctx.author) in strong:
-        n = randint(25, 75)
-        await ctx.send(f'drop and give me {n}')
-        strong[str(ctx.author)]['pushups'] += n
-        await update_log()
+    if os.getenv('DEVELOPMENT'):
+        await test_channel.send('```Daily Reset```')
     else:
-        await ctx.send('you are not yet a disciple of the iron temple')
+        await channel.send('```Daily Reset```')
 
 
-@client.command()
-async def give_pushups(ctx, target: discord.User, number):
-    """roll for a member to get pushups"""
-    if str(target) in strong:
-        strong[str(target)]['pushups'] += randint(10, 30)
-        await update_log()
+@daily_reset.before_loop
+async def init_loop():
+    await asyncio.sleep((schedule - datetime.now()).total_seconds())
+    roll_pushups().start()
 
+
+@ client.command()
+async def pushups(ctx, *args):
+    """get pushups or use a roll for someone else"""
+    if len(args) == 0:
+        if str(ctx.author) in strong:
+            n = randint(25, 75)
+            await ctx.send(f'drop and give me {n}')
+            strong[str(ctx.author)]['pushups'] += n
+            await update_log()
+        else:
+            await ctx.send('you are not yet a disciple of the iron temple')
     else:
-        await ctx.send('user is not a disciple of the iron temple')
+        target = discord.utils.get(client.get_all_members(), id=int(args[0][2:-1]))
+        if str(target) in strong and str(ctx.author) in strong \
+                and strong[str(ctx.author)]['rolls'] > 0:
+            n = randint(10, 30)
+            strong[str(target)]['pushups'] += n
+            strong[str(ctx.author)]['rolls'] -= 1
+            await ctx.send(f'{target.mention} has been assigned {n}')
+            await update_log()
+
+        else:
+            await ctx.send('user is not a disciple of the iron temple\n'
+                           'or you are out of rolls')
 
 
 @client.command()
@@ -83,6 +121,8 @@ async def signup(ctx):
         strong[str(ctx.author)] = {'rolls': 1,
                                    'pushups': 0}
         await update_log()
+    else:
+        await ctx.send('you are already a member of the iron temple')
 
 
 @client.command()
@@ -96,11 +136,23 @@ async def remove(ctx):
 
 @client.command()
 async def leaderboard(ctx):
+    """display a daily leaderboard of temple members"""
     if not len(strong):
         await ctx.send('no disciples to display')
     else:
         sorted_strong = dict(sorted(strong.items(), key=lambda item: not item[1]['pushups']))
         await ctx.send('\n'.join([k + ': ' + str(sorted_strong[k]['pushups']) for k in sorted_strong]))
+
+
+@client.event
+async def on_ready():
+    global initialized
+    if initialized:
+        return
+    daily_reset.start()
+    client.on_message = on_message
+    print(client.guilds)
+    initialized = True
 
 
 client.run(token)
